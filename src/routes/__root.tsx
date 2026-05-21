83878d8033c64e814051cd86e3c38dcc644e2c1d
Marcio Gabriel Holm Monteiro <marciogholmm@gmail.com>
2026-05-21 14:39:03 -0300

    fix: adiciona Toaster do sonner no root (toasts estavam invisiveis)

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -10,6 +10,7 @@ import {
 
 import appCss from "../styles.css?url";
 import { ThemeProvider } from "@/components/nexus/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 function NotFoundComponent() {
   return (
@@ -125,6 +126,7 @@ function RootComponent() {
     <QueryClientProvider client={queryClient}>
       <ThemeProvider>
         <Outlet />
+        <Toaster richColors closeButton />
       </ThemeProvider>
     </QueryClientProvider>
   );
